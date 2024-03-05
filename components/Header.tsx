2cc58c51fb51771674c7e390496c878781a8234e
Clément Storne <clementstorne@gmail.com>
2024-03-05 19:43:03 +0100

    header tablet

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import clsx from "clsx";
 import Image from "next/image";
 import { useState } from "react";
 import Button from "./Button";
@@ -12,7 +13,12 @@ const Header = () => {
   };
 
   return (
-    <header className="w-full h-[110px] px-6 py-10 flex justify-between items-center fixed top-0 left-0 right-0">
+    <header
+      className={clsx(
+        "w-full h-27.5 px-6 py-10 flex justify-between items-center fixed top-0 left-0 right-0",
+        "md:h-44.5 md:pr-0 md:pl-9.75"
+      )}
+    >
       <Image
         src={"/creative.svg"}
         alt="Creative - Branding & website design agency"
@@ -28,7 +34,7 @@ const Header = () => {
           width={20}
           height={20}
           priority={true}
-          className="cursor-pointer"
+          className="cursor-pointer md:hidden"
           onClick={handleClickOnMenu}
         />
       ) : (
@@ -38,25 +44,45 @@ const Header = () => {
           width={24}
           height={18}
           priority={true}
-          className="cursor-pointer"
+          className="cursor-pointer md:hidden"
           onClick={handleClickOnMenu}
         />
       )}
 
-      {isMenuOpen ? (
-        <nav className="fixed top-[110px] right-[26px] bg-gray text-white pt-8 px-6.5 pb-7">
-          <ul className="flex flex-col justify-center items-center space-y-6">
-            <li>About</li>
-            <li>Service</li>
-            <li>Projects</li>
-            <li>
-              <Button variant="primary" className="px-11 py-5 text-sm">
-                Schedule a Call
-              </Button>
-            </li>
-          </ul>
-        </nav>
-      ) : null}
+      <nav
+        className={clsx(
+          isMenuOpen &&
+            "fixed top-27.5 right-6.5 bg-gray text-white pt-8 px-6.5 pb-7",
+          !isMenuOpen && "hidden",
+          "md:bg-red md:static md:w-109.5 md:h-44.5 md:py-14.25 md:pr-10 md:pl-8.25"
+        )}
+      >
+        <ul
+          className={clsx(
+            "flex flex-col justify-center items-center space-y-6 text-white",
+            "md:flex-row items-center md:space-y-0 md:space-x-5 md:text-xs"
+          )}
+        >
+          <li className="inline-block cursor-pointer hover:font-bold">About</li>
+          <li className="inline-block cursor-pointer hover:font-bold">
+            Service
+          </li>
+          <li className="inline-block cursor-pointer hover:font-bold">
+            Projects
+          </li>
+          <li>
+            <Button
+              variant="primary"
+              className={clsx(
+                "px-11 py-5 text-sm",
+                "md:bg-gray md:hover:bg-gray-hover md:ml-2 md:px-4.5 md:py-5.5"
+              )}
+            >
+              Schedule a Call
+            </Button>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,6 +16,7 @@ const config: Config = {
       "text-black": "#000000",
     },
     fontSize: {
+      xs: ["15px", "32px"],
       sm: ["16px", "19px"],
       base: ["18px", "32px"],
       lg: ["20px", "32px"],
@@ -24,10 +25,17 @@ const config: Config = {
     },
     extend: {
       spacing: {
+        "4.5": "18px",
         "5.5": "22px",
         "6.25": "25px",
         "6.5": "26px",
+        "8.25": "33px",
         "8.75": "35px",
+        "9.75": "39px",
+        "14.25": "57px",
+        "27.5": "110px",
+        "44.5": "178px",
+        "109.5": "438px",
       },
     },
   },
